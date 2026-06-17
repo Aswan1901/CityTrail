82bb945352be500461c0ef89b1beb2f055fb927c
Aswan1901 <aswan.josephmathieu1999@gmail.com>
2026-06-17 17:34:51 +0200

    cors

--- src/main/java/com/example/CityTrail/CityTrailApplication.java
+++ src/main/java/com/example/CityTrail/CityTrailApplication.java
@@ -2,6 +2,9 @@ package com.example.CityTrail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CityTrailApplication {
@@ -10,4 +13,17 @@ public class CityTrailApplication {
 		SpringApplication.run(CityTrailApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:8081")
+						.allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("Authorization", "Content-Type")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
